ff18ce08ed75ce4c5671e86878d6181b901ba192
Steven Kearnes <skearnes@gmail.com>
2022-10-10 21:07:45 -0400

    remove unused constant

--- ord_interface/client/ord_client.py
+++ ord_interface/client/ord_client.py
@@ -26,7 +26,6 @@ from ord_schema.proto import reaction_pb2
 from ord_interface.client import query
 
 TARGET = "https://client.open-reaction-database.org"
-ORD_DATA_URL = "https://github.com/Open-Reaction-Database/ord-data/raw/main/"
 
 
 class OrdClient:
