257d5938cfa7f7bd741cd3ca3ebe4382f723bfd8
Michael Kennel <mikennel@Michaels-MBP-2.attlocal.net>
2022-10-26 13:26:10 -0400

    scaffolding react router

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/">
+
+          </Route>
+          <Route exact path="/client/browse">
+
+          </Route>
+          <Route exact path="/search">
+
+          </Route>
+          <Route exact path="/editor/datasets">
+
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
