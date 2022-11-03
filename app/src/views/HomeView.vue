e796a6ee661cf8c5be346305f612f3f0df17e4e2
Michael Kennel <mikennel@Michaels-MBP-2.attlocal.net>
2022-11-03 13:23:12 -0400

    vue set up

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.home
+  img( alt="Vue logo" src="../assets/logo.png")
+  HelloWorld(msg="Welcome to Your Vue.js App")
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
