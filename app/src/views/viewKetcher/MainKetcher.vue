87f7609fc344f9d5d56a98cf75330329eef84039
Michael Kennel <mikennel@dhcp-10-5-110-186.dhcp.nd.edu>
2022-11-14 14:07:39 -0500

    import ketcher js at mounted

--- app/src/views/viewKetcher/MainKetcher.vue
+++ app/src/views/viewKetcher/MainKetcher.vue
@@ -1,4 +1,7 @@
 <template lang="pug">
+//- This is a simple view which emulates the html file and pulls in the js and css files for the ketcher interface
+//- It expects a folder "ketcher" to be in the root of src with files extracted from:
+//- https://github.com/epam/ketcher/releases/download/v2.5.1/ketcher-standalone-2.5.1.zip
 #ketcher
   noscript You need to enable JavaScript to run this app.
   #root
@@ -9,11 +12,11 @@
 export default {
   mounted() {
     // import ketcher js in mounted so the dom is ready
-    import("@/views/ketcher/static/js/main.027562ee.js")
+    import("@/ketcher/static/js/main.027562ee.js")
   }
 }
 </script>
 
 <style lang="sass" scoped>
-@import '@/views/ketcher/static/css/main.8e693d51.css'
+@import '@/ketcher/static/css/main.8e693d51.css'
 </style>
