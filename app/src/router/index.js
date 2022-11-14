7e0530653e51e4b053c3d61299c7652d21633589
Michael Kennel <mikennel@dhcp-10-5-110-186.dhcp.nd.edu>
2022-11-14 13:55:25 -0500

    loading ketcher in vue

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -20,7 +20,7 @@ const routes = [
   {
     path: '/ketcher',
     name: 'ketcher',
-    component: () => import('../views/ketcher/index.html')
+    component: () => import('../views/viewKetcher/MainKetcher.vue')
   }
 ]
 
---
+++ app/src/views/viewKetcher/MainKetcher.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+#ketcher
+  noscript You need to enable JavaScript to run this app.
+  #root
+</template>
+
+<script>
+
+export default {
+  mounted() {
+    // import ketcher js in mounted so the dom is ready
+    import("@/views/ketcher/static/js/main.027562ee.js")
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import '@/views/ketcher/static/css/main.8e693d51.css'
+</style>
