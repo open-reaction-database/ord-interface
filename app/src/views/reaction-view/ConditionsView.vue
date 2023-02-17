b5d14542599b82578d41cbb95208ebf6cf007c31
Michael Kennel <mikennel@Michaels-MBP-2.attlocal.net>
2023-02-17 13:55:06 -0500

    conditions temp and pressure

--- app/src/views/reaction-view/ConditionsView.vue
+++ app/src/views/reaction-view/ConditionsView.vue
@@ -13,7 +13,27 @@ export default {
     },
     tempSetPoint () {
       const setpoint = this.conditions.temperature.setpoint
-      return `${setpoint.value} (± ${setpoint.precision}) ${setpoint.units}`
+      if (!setpoint) return "None"
+      const tempUnits = reaction_pb.Temperature.TemperatureUnit
+      const tempUnit = Object.keys(tempUnits).find(key => tempUnits[key] == setpoint.units)
+      return `${setpoint.value} (± ${setpoint.precision}) ${tempUnit.toLowerCase()}`
+    },
+    pressureType () {
+      const controlTypes = reaction_pb.PressureConditions.PressureControl.PressureControlType
+      return Object.keys(controlTypes).find(key => controlTypes[key] == this.conditions.pressure.control.type)
+    },
+    pressureSetPoint () {
+      const setpoint = this.conditions.pressure.setpoint
+      if (!setpoint) return "None"
+      const pressureUnits = reaction_pb.Pressure.PressureUnit
+      const pressureUnit = Object.keys(pressureUnits).find(key => pressureUnits[key] == setpoint.units)
+      return `${setpoint.value} (± ${setpoint.precision}) ${pressureUnit.toLowerCase()}`
+    },
+    pressureAtmo () {
+      const atmo = this.conditions.pressure.atmosphere
+      const atmoTypes = reaction_pb.PressureConditions.Atmosphere.AtmosphereType
+      const atmoType = Object.keys(atmoTypes).find(key => atmoTypes[key] == atmo.type)
+      return `${atmoType}${atmo.details ? `, ${atmo.details}` : ""}`
     }
   },
 }
@@ -21,11 +41,35 @@ export default {
 
 <template lang="pug">
 .conditions-view
-  .temperature.details(v-if='display=="temperature"')
+  .temperature.details(v-if='display==="temperature"')
     .label Control Type
     .value {{tempType}}
+    template(v-if='conditions.temperature.control.details')
+      .label Details
+      .value {{conditions.temperature.control.details}}
     .label Setpoint
     .value {{tempSetPoint}}
+    // TODO Flesh out temp measurements
+    template(v-if='conditions.temperature.measurementsList?.length')
+      .label Measurements
+      .value {{conditions.temperature.measurementsList}}
+  .pressure.details(v-if='display==="pressure"')
+    .label Control Type
+    .value {{pressureType}}
+    template(v-if='conditions.pressure.control.details')
+      .label Details
+      .value {{conditions.pressure.control.details}}
+    .label Setpoint
+    .value {{pressureSetPoint}}
+    .label Atmosphere
+    .value {{pressureAtmo}}
+    // TODO Flesh out pressure measurements
+    template(v-if='conditions.pressure.measurementsList?.length')
+      .label Measurements
+      .value {{conditions.pressure.measurementsList}}
+  //- .stirring.details(v-if='display==="stirring"')
+
+
 </template>
 
 <style lang="sass" scoped>
