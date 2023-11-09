125cfa52ce407a1255092c288c8e198dd4397302
Steven Kearnes <skearnes@relaytx.com>
2023-11-09 11:13:06 -0500

    format

--- app/src/utils/outcomes.js
+++ app/src/utils/outcomes.js
@@ -18,11 +18,12 @@ import reaction_pb from "ord-schema"
 
 export default {
   formattedTime(timeData) {
-    if (!timeData) return null
-    const timeUnits = reaction_pb.Time.TimeUnit
-    const type =
-        Object.keys(timeUnits).find(key => timeUnits[key] == timeData.units)
-    return `${timeData.value} ${type.toLowerCase()}${
-        timeData.units !== 0 ? "(s)" : ""}`
+    if (!timeData)
+      return null
+      const timeUnits = reaction_pb.Time.TimeUnit
+      const type =
+          Object.keys(timeUnits).find(key => timeUnits[key] == timeData.units)
+      return `${timeData.value} ${type.toLowerCase()}${
+          timeData.units !== 0 ? "(s)" : ""}`
   }
 }
