eb26cc9f1db001c3511fc407e0fe40cbc13f3db4
Steven Kearnes <skearnes@gmail.com>
2024-06-19 21:32:42 -0400

    bump ord-schema

--- ord_interface/api/testing.py
+++ ord_interface/api/testing.py
@@ -17,7 +17,7 @@ import os
 from glob import glob
 
 from ord_schema.message_helpers import load_message
-from ord_schema.orm.database import add_dataset, prepare_database, update_rdkit_ids, update_rdkit_tables
+from ord_schema.orm.database import add_dataset, prepare_database
 from ord_schema.proto import dataset_pb2
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
@@ -34,10 +34,5 @@ def setup_test_postgres(url: str) -> None:
     rdkit_cartridge = prepare_database(engine)
     with Session(engine) as session:
         for dataset in datasets:
-            add_dataset(dataset, session)
-            if rdkit_cartridge:
-                session.flush()
-                update_rdkit_tables(dataset.dataset_id, session)
-                session.flush()
-                update_rdkit_ids(dataset.dataset_id, session)
+            add_dataset(dataset, session, rdkit_cartridge=rdkit_cartridge)
             session.commit()
