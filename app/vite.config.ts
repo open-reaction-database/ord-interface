0df0e459c2f808063949db4b1c4e3da56620a52c
Michael Kennel <mikennel@dhcp-10-33-148-20.dhcp.nd.edu>
2025-09-23 15:34:39 -0400

    initializing react

---
+++ app/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8080, // parity with old vue app
+    proxy: {
+      '/api': {
+        target: 'http://0.0.0.0:5000',
+        changeOrigin: true
+      }
+    }
+  }
+})
